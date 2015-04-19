176db4bf056c4d761fc56d6da40b35a8804c30ad
paulrosen <paulrosen@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-19 03:05:17 +0000

    Upgrade to abcjs library 2.0
    git-svn-id: https://plugins.svn.wordpress.org/abc-notation/tags/2.0@1138313 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ abc-notation.php
@@ -0,0 +1,61 @@
+<?php
+/*
+Plugin Name: ABC Notation
+Plugin URI: http://wordpress.paulrosen.net/plugins/abc-notation
+Description: Include sheet music on your WordPress site by simply specifying the ABC style string in the shortcode <strong>[abcjs]</strong>. For a complete description of the syntax, see the <a href="http://wordpress.paulrosen.net/plugins/abc-notation">Plugin Site</a>.
+Version: 1.12.1
+Author: Paul Rosen
+Author URI: http://paulrosen.net
+License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+*/
+
+/*
+Copyright (C) 2015 Paul Rosen
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+//-- Add the javascript.
+add_action('wp_enqueue_scripts','abcjs_loader');
+
+function abcjs_loader() {
+	wp_enqueue_script( 'abcjs-plugin', plugins_url( '/abcjs_basic_2.0-min.js', __FILE__ ));
+}
+
+//-- Interpret the [abcjs] shortcode
+function create_music( $atts, $content ) {
+	$a = shortcode_atts( array(
+		'class' => 'abc-paper',
+		'parser' => '{}',
+		'engraver' => '{}'
+	), $atts );
+
+	$content2 = preg_replace("&<br />\n&", "\x01", $content);
+	$content2 = preg_replace("-&#8221;-", "\\\"", $content2);
+	$content2 = preg_replace("-&#8217;-", "'", $content2);
+	$content2 = preg_replace("-&#8243;-", "\\\"", $content2);
+	$content2 = preg_replace("-&#8220;-", "\\\"", $content2);
+
+	$id = 'abc-paper-' . uniqid();
+	$output = '<div id="' . $id . '" class="' . $a['class'] . '"></div>' .
+		'<script type="text/javascript">' .
+		'ABCJS.renderAbc("' . $id . '", "' . $content2 . '".replace(/\x01/g,"\n"), ' . $a['parser'] . ', ' . $a['engraver'] . ');' .
+		'</script>';
+
+	return $output;
+}
+add_shortcode( 'abcjs', 'create_music' );
---
+++ readme.txt
@@ -0,0 +1,55 @@
+=== ABC Notation ===
+Contributors: paulrosen
+Donate link: http://wordpress.paulrosen.net/plugins/abc-notation
+Tags: music abc-notation sheet-music abcjs
+Requires at least: 4.0
+Tested up to: 4.1
+Stable tag: 2.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Include sheet music on your WordPress site by simply specifying the ABC style string.
+
+== Description ==
+
+This includes the abcjs system on your WordPress site. To produce sheet music, put a valid ABC Notation string between the shortcodes [abcjs] and [/abcjs] on your page or post.
+
+== Installation ==
+
+1. Upload this folder to the `/wp-content/plugins/` directory.
+1. Activate the plugin through the 'Plugins' menu in WordPress.
+1. Click 'Settings' in the dashboard, then 'ABC Notation' to customize.
+
+== Frequently Asked Questions ==
+
+= Where can this be used? =
+
+Anywhere that shortcodes are accepted. That is, on pages, post, and widgets. It will not work on comments.
+
+= How does it work? =
+
+The plugin includes the [abcjs](http://abcjs.net/) JavaScript library. The string that is put in the shortcode is passed to that library, which translates it and renders it in an SVG element that it places on the page instead of the shortcode.
+
+= What can be put in the ABC string that is placed in the shortcode? =
+
+There is much written about ABC Notation around the web. You can start [here](http://abcnotation.com)
+
+== Upgrade Notice ==
+
+= 2.0 =
+* Upgrade to use the abcjs 2.0 code.
+
+= 1.12.1 =
+* Get rid of smart quotes.
+
+= 1.12 =
+* Initial version
+
+== Screenshots ==
+
+1. An example of a shortcode and the resultant music that is produced.
+
+== Changelog ==
+
+= 1.12 =
+* Initial version of this plugin. (Version numbers are in sync with the version of abcjs that is included.)
